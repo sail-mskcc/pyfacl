62006991588c7878108edf6d9f50af09906e7f2a
tobiaspk <tobiaspk1@gmail.com>
2026-02-10 10:58:05 -0500

    Refactor to module

--- pyfacl/cli.py
+++ pyfacl/cli.py
@@ -1,8 +1,6 @@
-import os
-
 import typer
 
-from pyfacl import FACL, FACLTrace
+from pyfacl import FACLHas, FACLTrace
 
 app = typer.Typer(
     help="pyfacl: A tool to manage and analyze POSIX file ACLs.", no_args_is_help=True
@@ -47,34 +45,14 @@ def permission_has(
     Check if user/group can navigate to path (--x), and specified ACL granted.
     """
     # get trace and final paths
-    facl_trace = FACLTrace(path=os.path.dirname(path), v=1)
-    facl_path = FACL(path=path, v=1)
-
-    # replace acl with --x for navigation check
-    acl_nav = ":".join(acl.split(":")[:-1] + ["--x"])
-    can_navigate = facl_trace.has_permission(acl_nav, "at_least")
-    has_permission = facl_path.has_permission(acl, mode)
-    if can_navigate and has_permission:
-        msg = (
-            f"User/group can navigate to '{path}'\n"
-            f"User/group has '{mode}' permission for ACL '{acl}'."
-        )
-    elif not can_navigate and has_permission:
-        msg = (
-            f"User/group cannot navigate to '{path}'\n"
-            f"User/group has '{mode}' permission for ACL '{acl}'."
-        )
-    elif can_navigate and not has_permission:
-        msg = (
-            f"User/group can navigate to '{path}'\n"
-            f"User/group does NOT have '{mode}' permission for ACL '{acl}'."
-        )
+    facl_has = FACLHas(path=path, v=1)
+    has_permission = facl_has.has_permission(acl, mode)
+    if has_permission:
+        typer.echo(f"Permission '{mode}' for ACL '{acl}' is granted on path '{path}'.")
     else:
-        msg = (
-            f"User/group cannot navigate to '{path}'\n"
-            f"User/group does NOT have '{mode}' permission for ACL '{acl}'."
+        typer.echo(
+            f"Permission '{mode}' for ACL '{acl}' is NOT granted on path '{path}'."
         )
-    typer.echo(msg)
 
 
 def main():
